130bdbfff999a878952301f7d191fa15d992091f
jujuwon <webmon@kakao.com>
2022-11-29 14:57:38 +0900

    feat: initial commit

---
+++ src/main/java/com/snowballer/api/SnowballerApplication.java
@@ -0,0 +1,13 @@
+package com.snowballer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowballerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowballerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowballer/api/SnowballerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowballer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowballerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
