0941e2ac69cd28ba06b114f1fd9ce8f094309c24
isieun0908 <supiya0908@naver.com>
2022-12-17 05:33:11 +0900

    Feat: 눈사람 조회하기 서비스 로직 작성 (#4) - TownSnowmanRepository - TownSnowmanService

---
+++ src/main/java/com/snowballer/api/service/TownSnowmanService.java
@@ -0,0 +1,34 @@
+package com.snowballer.api.service;
+
+import com.snowballer.api.domain.TownSnowman;
+import com.snowballer.api.dto.response.TownSnowmanResponse;
+import com.snowballer.api.repository.TownSnowmanRepository;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TownSnowmanService {
+
+    private final TownSnowmanRepository townSnowmanRepository;
+
+    public TownSnowmanResponse getLetter(Long id) {
+
+        // 권한 확인 (본인 마을의 눈사람인지)
+
+        // townSnowman에서 조회
+        TownSnowman townSnowman = townSnowmanRepository.findById(id)
+            .orElseThrow(()  -> new NoSuchElementException("Snowballers Error: 접근할 수 없는 눈사람입니다."));
+
+        // "haveLetter == True" 확인
+        townSnowman.checkHaveLetter();
+
+        // 만약, seen == false일 시, true로 값 변경
+        townSnowman.changeSeen();
+        townSnowmanRepository.save(townSnowman);
+
+        // dto 생성 및 반환
+        return TownSnowmanResponse.toResponse(townSnowman);
+    }
+}
