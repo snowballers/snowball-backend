216f240a230f5938ed017dafa855724e163a5983
isieun0908 <supiya0908@naver.com>
2022-12-17 06:29:07 +0900

    Feat: 편지 작성하기 서비스 로직 작성 - TownSnowmanService

--- src/main/java/com/snowballer/api/service/TownSnowmanService.java
+++ src/main/java/com/snowballer/api/service/TownSnowmanService.java
@@ -1,6 +1,7 @@
 package com.snowballer.api.service;
 
 import com.snowballer.api.domain.TownSnowman;
+import com.snowballer.api.dto.request.SubmitLetterRequest;
 import com.snowballer.api.dto.response.TownSnowmanResponse;
 import com.snowballer.api.repository.TownSnowmanRepository;
 import java.util.NoSuchElementException;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TownSnowmanService {
 
+    private final UrlService urlService;
     private final TownSnowmanRepository townSnowmanRepository;
 
     public TownSnowmanResponse getLetter(Long id) {
@@ -30,5 +32,19 @@ public class TownSnowmanService {
 
         // dto 생성 및 반환
         return TownSnowmanResponse.toResponse(townSnowman);
+    }
+
+    public void setLetter(String url, SubmitLetterRequest submitLetterRequest) {
+
+        // townSnowman 조회
+        TownSnowman townSnowman = townSnowmanRepository.findById(submitLetterRequest.getSnowmanId())
+            .orElseThrow(() -> new NoSuchElementException("Snowballers Error: 접근할 수 없는 눈사람입니다."));
+
+        // url을 townId로 변환
+        Long townId = urlService.decoding(url);
+
+        // townSnowman 값 입력
+        townSnowman.writeLetter(townId, submitLetterRequest);
+        townSnowmanRepository.save(townSnowman);
     }
 }
