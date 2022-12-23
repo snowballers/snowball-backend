2dbd9fba6a5a9529198ee75ba7e88b8d7cc7efab
isieun0908 <supiya0908@naver.com>
2022-12-24 06:53:46 +0900

    fix: 권한 확인 수정 (#27) - currentUser, user 값 출력

--- src/main/java/com/snowballer/api/security/SecurityUtil.java
+++ src/main/java/com/snowballer/api/security/SecurityUtil.java
@@ -7,8 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SecurityUtil {
 
