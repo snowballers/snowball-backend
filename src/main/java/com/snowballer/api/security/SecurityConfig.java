b0b3e5a2fbbf128d7a3526c9c075d1c000bd83c7
isieun0908 <supiya0908@naver.com>
2022-12-24 08:17:37 +0900

    fix: cors 허용범위 변경 - 전체 오리진 허용하도록 변경 - 전체 메소드 허용하도록 변경 - 전체 헤더 허용하도록 변경

--- src/main/java/com/snowballer/api/security/SecurityConfig.java
+++ src/main/java/com/snowballer/api/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedOrigin("*");
 		configuration.addAllowedMethod("*");
 		configuration.addAllowedHeader("*");
 		// configuration.addAllowedHeader("*");
